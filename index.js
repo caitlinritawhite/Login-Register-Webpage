35b441c8933c43630f39132bdbdcd6410638ca04
Caitlin White <caitlinwhite@Caitlins-MacBook-Air.local>
2021-02-08 11:36:50 -0500

    deploying bugs

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require("express");
+// const express = require("express");
 const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
 require('dotenv').config()
 
-const app = express();
+// const app = express();
 const typeDefs = require('./graphQL/typeDefs');
 const resolvers = require('./graphQL/resolvers');
 // const { MONGODB } = require('./config');
@@ -13,12 +13,12 @@ const server = new ApolloServer({
   resolvers
 });
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// // Serve up static assets (usually on heroku)
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
 
 
 
